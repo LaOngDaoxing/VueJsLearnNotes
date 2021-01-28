73ee43431303a7fc38d5f61bb104a0273e3c6a9c
LaOngDaoxing <18731197300@163.com>
2021-01-28 14:21:01 +0800

    数组Arr.reduce(第一个参数是回调函数,第二个参数一般初始值为0)

--- static/document/js/math/高阶函数.js
+++ static/document/js/math/高阶函数.js
@@ -3,12 +3,12 @@
                   （2）需求:将所有小于100的数字进行转化: 全部*2
                   （3）需求:将所有new2Nums数字相加,得到最终的结果
  * @Use高阶函数：
-            1、数组Arr.filter(回调函数)
+            1、数组Arr.filter(第一个参数是回调函数)
               其回调函数有一个要求: 必须返回一个boolean值
               true: 当返回true时, 函数内部会自动将这次回调的n加入到新的数组中
               false: 当返回false时, 函数内部会过滤掉这次的n
-            2、数组Arr.map(回调函数)
-            3、数组Arr.reduce(回调函数)
+            2、数组Arr.map(第一个参数是回调函数)
+            3、数组Arr.reduce(第一个参数是回调函数,第二个参数一般初始值为0)
               作用：对数组中所有的内容进行汇总
  *@代码示例：              
       let newNumArr = oldNumArr.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n);
@@ -34,6 +34,12 @@ let newNumArr1 = new2Nums.reduce(function (preValue, n) {
   // （3）需求:将所有new2Nums数字相加,得到最终的结果
   return preValue + n
 }, 0)
+// ————注解reduce执行过程
+// 第一次: preValue 0,n 20
+// 第二次: preValue 20,n 40
+// 第二次: preValue 60,n 80
+// 第二次: preValue 140,n 100
+// ————240
 console.log("一、演示过程结果--"+newNumArr1);
 
 /**---------------------------- 二、完整代码 ----------------------------*/
