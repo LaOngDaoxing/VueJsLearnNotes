e21790019e2807e2d211d8df8c86997f21c9d234
LaOngDaoxing <18731197300@163.com>
2021-01-28 14:04:56 +0800

    高阶函数

--- static/document/js/math/01-函数式编程(面向函数).js
+++ static/document/js/math/01-函数式编程(面向函数).js
@@ -1,4 +1,4 @@
-// 1.需求: 取出所有小于100的数字
+// （1）需求: 取出所有小于100的数字
 let newNums = []
 for (let n of nums) {
   if (n < 100) {
@@ -6,7 +6,7 @@ for (let n of nums) {
   }
 }
 
-// 2.需求:将所有小于100的数字进行转化: 全部*2
+// （2）需求:将所有小于100的数字进行转化: 全部*2
 let new2Nums = []
 for (let n of newNums) {
   new2Nums.push(n * 2)
@@ -14,7 +14,7 @@ for (let n of newNums) {
 console.log(new2Nums);
 
 
-// 3.需求:将所有new2Nums数字相加,得到最终的记过
+// （3）需求:将所有new2Nums数字相加,得到最终的结果
 let total = 0
 for (let n of new2Nums) {
   total += n
--- static/document/js/math/高阶函数.js
+++ static/document/js/math/高阶函数.js
@@ -1,44 +1,56 @@
 /**
- * @Description：高阶函数filter/map/reduce
- * @Remark：
-          filter中的回调函数有一个要求: 必须返回一个boolean值
-          true: 当返回true时, 函数内部会自动将这次回调的n加入到新的数组中
-          false: 当返回false时, 函数内部会过滤掉这次的n
+ * @Description： （1）取出所有小于100的数字
+                  （2）需求:将所有小于100的数字进行转化: 全部*2
+                  （3）需求:将所有new2Nums数字相加,得到最终的结果
+ * @Use高阶函数：
+            1、filter
+              其回调函数有一个要求: 必须返回一个boolean值
+              true: 当返回true时, 函数内部会自动将这次回调的n加入到新的数组中
+              false: 当返回false时, 函数内部会过滤掉这次的n
+            2、map
+            3、reduce
+              作用：对数组中所有的内容进行汇总
  */
-const nums = [10, 20, 111, 222, 444, 40, 50]
+const oldNumArr = [10, 20, 111, 222, 444, 40, 50]
 
-// let total = nums.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n);
-// console.log(total);
+/**---------------------------- 一、演示过程 ----------------------------*/
+// 10, 20, 40, 50
+let newNums = oldNumArr.filter(function (n) {
+  // （1）需求: 取出所有小于100的数字
+  return n < 100
+})
+// console.log(newNums);
 
-let total = nums.filter(function (n) {
+// 20, 40, 80, 100
+let new2Nums = newNums.map(function (n) { // 20
+  // （2）需求:将所有小于100的数字进行转化: 全部*2
+  return n * 2
+})
+console.log(new2Nums);
+
+let newNumArr1 = new2Nums.reduce(function (preValue, n) {
+  // （3）需求:将所有new2Nums数字相加,得到最终的结果
+  return preValue + n
+}, 0)
+console.log("一、演示过程结果--"+newNumArr1);
+
+
+// let newNumArr = oldNumArr.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n);
+// console.log(newNumArr);
+/**---------------------------- 二、完整代码 ----------------------------*/
+let newNumArr2 = oldNumArr.filter(function (n) {
+  // （1）需求: 取出所有小于100的数字
   return n < 100
 }).map(function (n) {
+  // （2）需求:将所有小于100的数字进行转化: 全部*2
   return n * 2
 }).reduce(function (prevValue, n) {
+  // （3）需求:将所有new2Nums数字相加,得到最终的结果
   return prevValue + n
 }, 0)
-console.log(total);
+console.log("二、完整代码结果--"+newNumArr2);
 
-// 1.filter函数的使用
-// // 10, 20, 40, 50
-// let newNums = nums.filter(function (n) {
-//   return n < 100
-// })
-// // console.log(newNums);
-//
-// // 2.map函数的使用
-// // 20, 40, 80, 100
-// let new2Nums = newNums.map(function (n) { // 20
-//   return n * 2
-// })
-// console.log(new2Nums);
-//
-// // 3.reduce函数的使用
-// // reduce作用对数组中所有的内容进行汇总
-// let total = new2Nums.reduce(function (preValue, n) {
-//   return preValue + n
-// }, 0)
-// console.log(total);
+
 
 // 第一次: preValue 0 n 20
 // 第二次: preValue 20 n 40
